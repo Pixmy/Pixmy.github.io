8b46e1132ca4703c20904684ab51d9d38c725b24
Pixmy <pixmy@outlook.com>
2016-12-20 12:24:36 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+//Step1
+ $db = mysqli_connect('mysql8.000webhost.com','a2931638_arturo','','a2931638_arturo')
+ or die('Error connecting to MySQL server.');
+?>
+
+<html>
+ <head>
+ </head>
+ <body>
+ <h1>PHP connect to MySQL</h1>
+ <?php
+//Step2
+$query = "SELECT * FROM comments";
+mysqli_query($db, $query) or die('Error querying database.');
+?>
+</body>
+</html>
